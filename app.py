d7d87cb3be99886a23be17f86d838bc0e4185134
Devaaws <143406616+Devaaws@users.noreply.github.com>
2025-01-05 10:56:47 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, World! This Flask app is running on port 5000."
+    return "second deployment deva ."
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000)
